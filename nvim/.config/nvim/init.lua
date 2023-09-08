8426305fc68cede249bb999a9f1754fb925b8394
Juan José <juan.ferrer@flowinc.com>
2023-09-08 09:30:56 -0600

    update config

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -592,6 +592,21 @@ lsp.ensure_installed({
   'vuels',
 })
 
+lsp.format_on_save({
+  format_opts = {
+    async = false,
+    timeout_ms = 10000,
+  },
+  servers = {
+    ['lua_ls'] = {'lua'},
+    ['rust_analyzer'] = {'rust'},
+    ['prettier'] = {'svelte'},
+    -- if you have a working setup with null-ls
+    -- you can specify filetypes it can format.
+    -- ['null-ls'] = {'javascript', 'typescript'},
+  }
+})
+
 lsp.setup()
 
 --vim.o.runtimepath = vim.o.runtimepath .. ",~/workspace/nvim-plugins/nvim-gtd-planner"
