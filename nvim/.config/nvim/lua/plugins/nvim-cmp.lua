0b72b7bc8a6b7b174bc3efc56fafb5aa6dec7ba6
Juan José Ruiz Ferrer <juanjose@realitat.com.mx>
2025-01-20 18:22:29 -0600

    update nvim and tmux

--- nvim/.config/nvim/lua/plugins/nvim-cmp.lua
+++ nvim/.config/nvim/lua/plugins/nvim-cmp.lua
@@ -1,1 +1,45 @@
-return { 'hrsh7th/nvim-cmp' }
+return {
+    'hrsh7th/nvim-cmp',
+    config = function()
+        require('cmp').setup({
+            sources = {
+                { name = "codeium" },
+                { name = "nvim_lsp" },
+                { name = "path" },
+            },
+            symbol_map = {
+                Codeium = "",
+                Text = "",
+                Method = "ƒ",
+                Function = "",
+                Constructor = "",
+                Field = "",
+                Variable = "",
+                Class = "",
+                Interface = "ﰮ",
+                Module = "",
+                Property = "",
+                Unit = "",
+                Value = "",
+                Enum = "",
+                Keyword = "",
+                Snippet = "",
+                Color = "",
+                File = "",
+                Reference = "",
+                Folder = "",
+                EnumMember = "",
+                Constant = "",
+                Struct = "",
+            },
+            formatting = {
+                format = require('lspkind').cmp_format({
+                    mode = "symbol",
+                    maxwidth = 50,
+                    ellipsis_char = '...',
+                    symbol_map = { Codeium = "", }
+                })
+            }
+        })
+    end
+}
