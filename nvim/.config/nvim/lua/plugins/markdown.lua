b500f8e1d6f83ef1e57ed9398dd0e94ec216c965
Juan José Ruiz Ferrer <juanjose@realitat.com.mx>
2025-01-22 17:55:49 -0600

    update markdown plugins for nvim

---
+++ nvim/.config/nvim/lua/plugins/markdown.lua
@@ -0,0 +1,10 @@
+return {
+    'MeanderingProgrammer/markdown.nvim',
+    main = "render-markdown",
+    opts = {},
+    name = "render-markdown",
+    dependencies = {
+        "nvim-treesitter/nvim-treesitter",
+        "nvim-tree/nvim-web-devicons"
+    }
+}
