a7358302972f79fda6620e3113f66d21bf4afc16
Juan Jose Ruiz Ferrer <jjruiz@change.org>
2026-04-23 17:37:40 -0600

    update nvim treesitter

--- nvim/.config/nvim/lua/plugins/nvim-treesitter.lua
+++ nvim/.config/nvim/lua/plugins/nvim-treesitter.lua
@@ -10,29 +10,63 @@ return {
         local configs = require('nvim-treesitter.configs')
         configs.setup({
             ensure_installed = {
-                "c",
-                "lua",
-                "vim",
-                "query",
-                "ruby",
-                "javascript",
-                "typescript",
-                "html",
-                "css",
-                "dockerfile",
-                "markdown",
-                "rust",
-                "svelte",
-                "regex",
-                "graphql",
-                "python",
-                "go",
-                "elixir"
+                "bash", "c", "css", "dockerfile", "elixir", "go", "graphql",
+                "html", "javascript", "json", "lua", "markdown",
+                "markdown_inline", "python", "query", "regex", "ruby", "rust",
+                "svelte", "tsx", "typescript", "vim", "yaml"
             },
-            sync_install = true,
+            sync_install = false,
             auto_install = true,
             highlight = { enable = true, disable = { 'NvimTree' } },
             indent = { enable = true },
+            endwise = { enable = true },
+            incremental_selection = {
+                enable = true,
+                keymaps = {
+                    init_selection = "<CR>",
+                    node_incremental = "<CR>",
+                    node_decremental = "<BS>",
+                },
+            },
+            textobjects = {
+                select = {
+                    enable = true,
+                    lookahead = true,
+                    keymaps = {
+                        ["af"] = "@function.outer",
+                        ["if"] = "@function.inner",
+                        ["ac"] = "@class.outer",
+                        ["ic"] = "@class.inner",
+                        ["aa"] = "@parameter.outer",
+                        ["ia"] = "@parameter.inner",
+                    },
+                },
+                move = {
+                    enable = true,
+                    set_jumps = true,
+                    goto_next_start = {
+                        ["]m"] = "@function.outer",
+                    },
+                    goto_next_end = {
+                        ["]M"] = "@function.outer",
+                    },
+                    goto_previous_start = {
+                        ["[m"] = "@function.outer",
+                    },
+                    goto_previous_end = {
+                        ["[M"] = "@function.outer",
+                    },
+                },
+                swap = {
+                    enable = true,
+                    swap_next = {
+                        ["<leader>a"] = "@parameter.inner",
+                    },
+                    swap_previous = {
+                        ["<leader>A"] = "@parameter.inner",
+                    },
+                },
+            },
         })
     end,
 }
